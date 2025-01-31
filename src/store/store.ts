47f1a903f669052c21b574d94aaab64f67ea5482
Whittun <whittun@gmail.com>
2025-02-01 02:38:37 +0300

    feat: fix paths

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import objectivesReducer from "../components/Objectives/ObjectivesSlice";
-import categoriesReducer from "../components/Categories/CategoriesSlice";
+import objectivesReducer from "../components/Objectives/ObjectivesSlice.ts";
+import categoriesReducer from "../components/Categories/CategoriesSlice.ts";
 
 export const store = configureStore({
   reducer: { objectives: objectivesReducer, categories: categoriesReducer },
