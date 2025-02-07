769ff8de904d53cec724066c05b6e7db44864fd8
Whittun <whittun@gmail.com>
2025-02-07 13:50:20 +0300

    build: add customizations for different environments

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_GITHUB_PAGES === "true" ? "/foundation/" : "/",
+  base: "/",
   server: {
     watch: {
       usePolling: true,
