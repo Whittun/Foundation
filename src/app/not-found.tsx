e7484e4b29bd8adfd65777ef94e126366b0a7cd5
Whittun <whittun@gmail.com>
2025-02-14 11:21:52 +0300

    feat: add custom 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col justify-center grow items-center">
+      <h1>404 - Page Not Found</h1>
+      <p>It seems we can't find what you're looking for.</p>
+    </div>
+  );
+}
