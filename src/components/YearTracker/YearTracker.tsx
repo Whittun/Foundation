3722b184a2d8a4f75e57293dc83f5209cfa782d4
Aleksandr <shpanov@strivepro.tech>
2026-03-21 11:00:58 +0300

    feat: Write the core logic for the calendar

--- src/components/YearTracker/YearTracker.tsx
+++ src/components/YearTracker/YearTracker.tsx
@@ -1,18 +1,15 @@
-export const YearTracker = () => {
-  const targetYear = 2026;
+import s from './YearTracker.module.css';
 
-  const currentDate = new Date(targetYear, 0, 1);
-  const processedDate = new Date(currentDate.getFullYear(), 0, 1);
+const zeroFormatted = (number: number) => {
+  return `${number}`.length < 2 ? `0${number}` : `${number}`;
+};
 
-  type yearMapType = Record<string, number>;
+const getYearMap = (targetYear: number) => {
+  const yearMap:  Record<string, number> = {};
 
-  const yearMap: yearMapType = {};
+  const processedDate = new Date(targetYear, 0, 1);   
 
-  const zeroFormatted = (number: number) => {
-    return `${number}`.length < 2 ? `0${number}` : `${number}`;
-  };
-
-  while (currentDate.getFullYear() === processedDate.getFullYear()) {
+  while (targetYear === processedDate.getFullYear()) {
     const year = processedDate.getFullYear();
     const month = processedDate.getMonth() + 1;
     const day = processedDate.getDate();
@@ -24,5 +21,70 @@ export const YearTracker = () => {
     processedDate.setDate(processedDate.getDate() + 1);
   }
 
-  return <p>{String(new Date())}</p>;
+  return yearMap;
+}
+
+const yearMap = getYearMap(2026);
+
+const dates = Object.keys(yearMap);
+
+type  CalendarDay = string | null;
+
+type Month = CalendarDay[];
+type Months = Month[];
+
+const months: Months = Array.from({length: 12}).map(() => []);
+
+dates.forEach((date) => {
+  const [year, month, day] = date.split('-');
+
+  const numIndexMonth = Number(month) - 1;
+
+  const weekIndex = ((new Date(date)).getDay() + 6) % 7;
+
+  if (months[numIndexMonth].length === 0) {
+    const monthOffset = Array.from({length: weekIndex}).map(() => null);
+    months[numIndexMonth].push(...monthOffset);
+  }
+
+  months[numIndexMonth].push(date);
+})
+
+const monthsNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+] as const;
+
+export const YearTracker = () => {
+  return <div className={s.year}>{months.map((month, index) => {
+
+    return (
+      <div className={s.month}>
+        <h2 className={s.monthName}>{monthsNames[index]}</h2>
+        {month.map((date) => { 
+
+          if (date === null) {  
+            return <p className={s.day}>
+              <span></span>
+            </p>
+          }
+          const [year, month, day] = date.split('-');
+
+          return <p className={s.day}>
+              <span>{day}</span>
+          </p>
+        })}
+      </div>
+    )
+  })}</div>;
 };
